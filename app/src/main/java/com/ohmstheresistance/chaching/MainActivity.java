9dc1763b5e8c897005f1521ed136a67009cd8a93
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-04-18 04:54:55 -0400

    Switched everything from MainActivity to MainFragment

--- app/src/main/java/com/ohmstheresistance/chaching/MainActivity.java
+++ app/src/main/java/com/ohmstheresistance/chaching/MainActivity.java
@@ -1,84 +1,25 @@
 package com.ohmstheresistance.chaching;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.util.Log;
 
-import com.ohmstheresistance.chaching.model.Country;
-import com.ohmstheresistance.chaching.network.CountryService;
-import com.ohmstheresistance.chaching.network.RetrofitSingleton;
-import com.ohmstheresistance.chaching.recyclerview.CountryAdapter;
+import com.ohmstheresistance.chaching.fragments.MainFragment;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
-
-    private static final String TAG = "CountryJSON.TAG";
-    private RecyclerView countryRecyclerView;
-    private SearchView citySearchView;
-    private CountryAdapter countryAdapter;
-    private List<Country> countryList = new ArrayList<>();
-
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        countryRecyclerView = findViewById(R.id.country_recycler_view);
-        citySearchView = findViewById(R.id.city_search_view);
-
-        Retrofit countryRetrofit = RetrofitSingleton.getRetrofitInstance();
-        CountryService countryService = countryRetrofit.create(CountryService.class);
-        countryService.getCountries().enqueue(new Callback<List<Country>>() {
-
-            @Override
-            public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
-
-                Log.d(TAG, "Country Retrofit Call Works: " + response.body().get(7).getCountry());
-                countryAdapter = new CountryAdapter(response.body());
-                countryRecyclerView.setAdapter(countryAdapter);
-                countryRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-                citySearchView.setOnQueryTextListener(MainActivity.this);
-
-                countryList = response.body();
-
-            }
-
-            @Override
-            public void onFailure(Call<List<Country>> call, Throwable t) {
-                Log.d(TAG, "Country Retrofit Call Failed: " + t.getMessage());
-
-            }
-
-        });
+        MainFragment mainFragment = new MainFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_container, mainFragment);
+        fragmentTransaction.commit();
 
     }
 
-    @Override
-    public boolean onQueryTextSubmit(String s) {
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String s) {
-        List<Country> newCountryList = new ArrayList<>();
-        for (Country country : countryList) {
-
-            if (country.getName().toLowerCase().contains(s.toLowerCase())) {
-                newCountryList.add(country);
-            }
-        }
-        countryAdapter.setData(newCountryList);
-        return false;
-    }
 }
--- app/src/main/java/com/ohmstheresistance/chaching/fragments/MainFragment.java
+++ app/src/main/java/com/ohmstheresistance/chaching/fragments/MainFragment.java
@@ -1,17 +1,41 @@
 package com.ohmstheresistance.chaching.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ohmstheresistance.chaching.MainActivity;
 import com.ohmstheresistance.chaching.R;
+import com.ohmstheresistance.chaching.model.Country;
+import com.ohmstheresistance.chaching.network.CountryService;
+import com.ohmstheresistance.chaching.network.RetrofitSingleton;
+import com.ohmstheresistance.chaching.recyclerview.CountryAdapter;
 
-public class MainFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainFragment extends Fragment implements SearchView.OnQueryTextListener{
 
     private View rootView;
+    private static final String TAG = "CountryJSON.TAG";
+    private RecyclerView countryRecyclerView;
+    private SearchView citySearchView;
+    private CountryAdapter countryAdapter;
+    private Context context;
+    private List<Country> countryList = new ArrayList<>();
 
     public MainFragment() {
         // Required empty public constructor
@@ -21,8 +45,54 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+       rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        countryRecyclerView = rootView.findViewById(R.id.country_recycler_view);
+        citySearchView = rootView.findViewById(R.id.city_search_view);
+
+        Retrofit countryRetrofit = RetrofitSingleton.getRetrofitInstance();
+        CountryService countryService = countryRetrofit.create(CountryService.class);
+        countryService.getCountries().enqueue(new Callback<List<Country>>() {
+
+            @Override
+            public void onResponse(Call<List<Country>> call, Response<List<Country>> response) {
+
+                Log.d(TAG, "Country Retrofit Call Works: " + response.body().get(7).getCountry());
+                countryAdapter = new CountryAdapter(response.body());
+                countryRecyclerView.setAdapter(countryAdapter);
+                countryRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+                citySearchView.setOnQueryTextListener(MainFragment.this);
+
+                countryList = response.body();
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Country>> call, Throwable t) {
+                Log.d(TAG, "Country Retrofit Call Failed: " + t.getMessage());
+
+            }
+
+        });
+        return rootView;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        List<Country> newCountryList = new ArrayList<>();
+        for (Country country : countryList) {
+
+            if (country.getName().toLowerCase().contains(s.toLowerCase())) {
+                newCountryList.add(country);
+            }
+        }
+        countryAdapter.setData(newCountryList);
+        return false;
     }
 
 }
